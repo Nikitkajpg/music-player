72316caa7427e4806667112dc322653f81899bed
Nikitkajpg <thetheddust@gmail.com>
2023-05-07 16:39:41 +0300

    Showing playlists

--- src/main/java/com/td/player/Controller.java
+++ src/main/java/com/td/player/Controller.java
@@ -1,17 +1,21 @@
 package com.td.player;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
 
 public class Controller {
     @FXML
-    private VBox musicListVBox, fileListVBox;
+    private VBox musicListVBox, fileListVBox, playlistVBox;
 
     private FileManager fileManager;
     private ArrayList<Music> musicArrayList;
+    private ArrayList<String> playlistArrayList;
+    private Accordion accordion;
 
     @FXML
     private void initialize() {
@@ -50,6 +54,24 @@ public class Controller {
 //                musicListVBox.getChildren().add(hBox);
 //            });
             musicListVBox.getChildren().add(new Label(music.getFileName().replace(".mp3", "")));
+        }
+        //----------------------------------------------
+        accordion = new Accordion();
+        playlistVBox.getChildren().add(accordion);
+        playlistArrayList = fileManager.getPlaylistArrayList();
+        String[] playlistNames = playlistArrayList.get(0).split("::");
+        for (String playlistName : playlistNames) {
+            accordion.getPanes().add(new TitledPane(playlistName, new VBox()));
+        }
+        for (int i = 1; i < playlistArrayList.size(); i++) {
+            for (int j = 0; j < accordion.getPanes().size(); j++) {
+                TitledPane titledPane = accordion.getPanes().get(j);
+                if (playlistArrayList.get(i).contains(titledPane.getText())) {
+                    String[] params = playlistArrayList.get(i).split("::");
+                    VBox vBox = (VBox) titledPane.getContent();
+                    vBox.getChildren().add(new Label(params[1]));
+                }
+            }
         }
     }
 
--- src/main/java/com/td/player/FileManager.java
+++ src/main/java/com/td/player/FileManager.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 public class FileManager {
     private boolean empty;
-    private ArrayList<String> pathArray;
+    private ArrayList<String> pathArray, playlistArrayList;
 
     private File dataDir, directoryListFile, playlistFile;
     private DirectoryChooser directoryChooser;
@@ -27,10 +27,12 @@ public class FileManager {
         pathArray = new ArrayList<>();
         directoryChooser = new DirectoryChooser();
         musicArrayList = new ArrayList<>();
+        playlistArrayList = new ArrayList<>();
 
         createDirectories();
     }
 
+    // при запуске программы создаются все необходимые папки и файлы
     private void createDirectories() {
         try {
             if (dataDir.mkdir()) {
@@ -40,36 +42,77 @@ public class FileManager {
             }
 
             if (directoryListFile.createNewFile()) {
-                System.out.println("file not exists");
+                System.out.println("directory file not exists");
             } else {
-                System.out.println("file exists");
-                checkIfFileIsEmpty();
+                System.out.println("directory file exists");
+                checkIfDirectoryListFileIsEmpty();
+            }
+
+            if (playlistFile.createNewFile()) {
+                System.out.println("playlist file not exist");
+            } else {
+                System.out.println("playlist file exists");
+                checkIfPlaylistFileIsEmpty();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void checkIfFileIsEmpty() {
+    private void checkIfPlaylistFileIsEmpty() {
+        try {
+            FileReader fileReader = new FileReader(playlistFile);
+            Scanner scanner = new Scanner(fileReader);
+            if (!scanner.hasNextLine()) {
+                System.out.println("playlist file is empty");
+                empty = true;
+
+                try {
+                    FileWriter fileWriter = new FileWriter(playlistFile);
+                    for (int i = 0; i < musicArrayList.size(); i++) {
+                        fileWriter.write(i + "::" + musicArrayList.get(i).getFileName() + "::All music" + "\n");
+                        //todo добавление в начало файла строки с низваниями плейлистов
+                    }
+                    fileWriter.flush();
+                    fileWriter.close();
+                    empty = false;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("playlist file is not empty");
+                empty = false;
+
+                // todo прочитать файл и вывести плейлисты на экран
+
+                while (scanner.hasNextLine()) {
+                    playlistArrayList.add(scanner.nextLine());
+                }
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkIfDirectoryListFileIsEmpty() {
         try {
             FileReader fileReader = new FileReader(directoryListFile);
             Scanner scanner = new Scanner(fileReader);
             if (!scanner.hasNextLine()) {
-                System.out.println("file is empty");
+                System.out.println("directory file is empty");
                 empty = true;
             } else {
-                System.out.println("file is not empty");
+                System.out.println("directory file is not empty");
                 empty = false;
 
                 while (scanner.hasNextLine()) {
-//                    pathArray.add(scanner.nextLine());
-                    updateArray(scanner.nextLine());
+                    updateArray(scanner.nextLine()); //todo исправить массив создание песен
                 }
-//                System.out.println(pathArray);
             }
             fileReader.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
@@ -139,5 +182,9 @@ public class FileManager {
 
     public ArrayList<String> getPathArray() {
         return pathArray;
+    }
+
+    public ArrayList<String> getPlaylistArrayList() {
+        return playlistArrayList;
     }
 }
