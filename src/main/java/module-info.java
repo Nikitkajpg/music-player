d325897184e8128b051f5f5cc6cb85329b83ed3c
Nikita <thetheddust@gmail.com>
2023-07-13 21:56:02 +0300

    More code improvements: more OOP

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,10 @@ module com.td.player {
 
     opens com.td.player to javafx.fxml;
     exports com.td.player;
+    exports com.td.player.elements;
+    opens com.td.player.elements to javafx.fxml;
+    exports com.td.player.managers;
+    opens com.td.player.managers to javafx.fxml;
+    exports com.td.player.controllers;
+    opens com.td.player.controllers to javafx.fxml;
 }
